6844151158f601c84204fa5fcc7169b04b65f78a
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2017-11-15 09:13:42 -0700

    anded winlose

--- assets/javascript/cargame.js
+++ assets/javascript/cargame.js
@@ -1,59 +1,79 @@
 // variables 
-var randomhp =[2279, 1909, 1376, 2113, 1006, 1273, 1210, 1069];
-var usersGadd = 0; 
-var randomnumber = 0;
+
+var usersGadd= 0; 
+var randomnumber= 0;
 var winC=0;
 var loseC=0;
-var choosenhp=[];
 $(document).ready(function()
 {	
-
-	//choosing the random number 
-			randomnumber = Math.floor((Math.random() * 200) + 19);
-			console.log(randomnumber);
-		//geteing var from html 
+	//geteing var from html 
 	function display()
 	{
 		document.getElementById("random#").innerHTML = randomnumber;
 		document.getElementById("usernumber").innerHTML = usersGadd;
+		document.getElementById("wins").innerHTML = winC;
+		document.getElementById("loses").innerHTML = loseC;
 	}
+	
+	//choosing the random number
+		randomnumber = Math.floor((Math.random() * 200) + 19);
+			console.log(randomnumber);
+				
+		miata = Math.floor((Math.random() * 3) + 1);
+		Porsche = Math.floor((Math.random() * 4) + 3);
+		demon = Math.floor((Math.random() * 4) + 6);
+		MP1 = Math.floor((Math.random() * 4) + 9);
+	
+
+
 	function click()
 	{
-		miata = Math.floor((Math.random() * 10) + 1);
-		Porsche = Math.floor((Math.random() * 10) + 1);
-		demon = Math.floor((Math.random() * 10) + 1);
-		MP1 = Math.floor((Math.random() * 10) + 1);
-
-
-	//making pics clickable 
+		//making pics clickable 
 		 $("#miata").on('click', function()
 			{
 				(usersGadd += miata)
 				display();
+				win();		
 			}); 				
 		$("#911").on('click',function()
 			{
 				(usersGadd += Porsche)
 				display();
+				win();
 			});	
 		$("#demon").on('click',function()
 			{
 				(usersGadd += demon)
 				display();
+				win();
 			});	
 		$("#p1").on('click',function()
 			{
 				(usersGadd += MP1)
 				display();
+				win();
 			});	
 	}
 //calling fuctions 
 display();
 click();
 	
-if(usersGadd == randomnumber)
+console.log(winC)
+console.log(loseC)
+function win()
 {
-	alert("win");
+	if(usersGadd === randomnumber)
+	{
+		alert("win");
+		(winC = winC+1);
+		display();
+	}
+	if(!(usersGadd<randomnumber))
+	{
+		alert("lose");
+		(loseC = loseC+1);
+		display();
+	}
 }
 
 //closing ready function
